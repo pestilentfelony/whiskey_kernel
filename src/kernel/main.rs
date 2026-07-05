fc3062653dba1bc7145214c3544b1cddb52c76a6
felony <pestilentfelony@protonmail.com>
2026-07-05 18:12:21 +0000

    not only ar e we riscv we are also based uart

--- src/kernel/main.rs
+++ src/kernel/main.rs
@@ -2,21 +2,24 @@
 #![no_main]
 
 mod uart;
-mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-use crate::uart::Uart;
-
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    Uart::new(0x1000_0000).write_str_raw("Hello, world!\n");
+    uart::init_uart();
+
+    println!("Hello World!");
+    
+    if let Some(uart) = uart::get_uart() {
+        uart.set_color(uart::COLOR_GREEN);
+        print!("Green text");
+        uart.reset_color();
+        println!("omg im green dadabebadabe");
+    }
 
     loop {}
 }
-
-
-
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
--- src/kernel/uart.rs
+++ src/kernel/uart.rs
@@ -1,3 +1,7 @@
+use core::fmt::{self, Write};
+use core::sync::atomic::Ordering;
+
+
 const UART_BASE: usize = 0x1000_0000;
 
 const THR: usize = 0; // Transmit Holding Register (write here to send)
@@ -5,7 +9,7 @@ const RBR: usize = 0; // Receive Buffer Register (read here to receive)
 const LSR: usize = 5; // Line Status Register
 
 const LSR_THRE: u8 = 1 << 5; // "Transmit Holding Register Empty" bit
-const LSR_DR: u8   = 1 << 0; // "Data Ready" bit
+const LSR_DR: u8 = 1 << 0; // "Data Ready" bit
 
 pub struct Uart {
     base: usize,
@@ -20,16 +24,12 @@ impl Uart {
         (self.base + offset) as *mut u8
     }
 
-
     pub fn write_byte(&self, byte: u8) {
         unsafe {
-            while core::ptr::read_volatile(self.reg(LSR)) & LSR_THRE == 0 {
-                // spin, contemplating your life choices
-            }
+            while core::ptr::read_volatile(self.reg(LSR)) & LSR_THRE == 0 {}
             core::ptr::write_volatile(self.reg(THR), byte);
         }
     }
-
 
     pub fn read_byte(&self) -> Option<u8> {
         unsafe {
@@ -41,17 +41,89 @@ impl Uart {
         }
     }
 
-    pub fn write_str_raw(&self, s: &str) {
+    fn write_ansi_code(&self, code: u8) {
+        self.write_byte(0x1b); // ESC
+        self.write_byte(b'[');
+        
+        if code >= 100 {
+            self.write_byte(b'1');
+            self.write_byte(b'0' + (code - 100) as u8);
+        } else {
+            self.write_byte(b'0' + (code / 10) as u8);
+            self.write_byte(b'0' + (code % 10) as u8);
+        }
+        
+        self.write_byte(b'm');
+    }
+
+    pub fn set_color(&self, color: u8) {
+        self.write_ansi_code(color);
+    }
+
+    pub fn reset_color(&self) {
+        self.write_ansi_code(0);
+    }
+}
+
+impl Write for Uart {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
         for byte in s.bytes() {
             self.write_byte(byte);
         }
-    }
-}
-
-
-impl core::fmt::Write for Uart {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        self.write_str_raw(s);
         Ok(())
     }
 }
+
+// Global UART instance
+static mut UART: Uart = Uart { base: UART_BASE };
+static UART_INITIALIZED: core::sync::atomic::AtomicBool = core::sync::atomic::AtomicBool::new(false);
+
+pub fn init_uart() {
+    UART_INITIALIZED.store(true, Ordering::Release);
+}
+
+pub fn get_uart() -> Option<&'static mut Uart> {
+    if UART_INITIALIZED.load(Ordering::Acquire) {
+        unsafe { Some(&mut UART) }
+    } else {
+        None
+    }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        if let Some(uart) = $crate::uart::get_uart() {
+            use core::fmt::Write;
+            let _ = write!(uart, $($arg)*);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::print!("\n");
+    };
+    ($($arg:tt)*) => {
+        $crate::print!("{}\n", format_args!($($arg)*));
+    };
+}
+
+// Color constants (ANSI codes)
+pub const COLOR_BLACK: u8 = 30;
+pub const COLOR_RED: u8 = 31;
+pub const COLOR_GREEN: u8 = 32;
+pub const COLOR_YELLOW: u8 = 33;
+pub const COLOR_BLUE: u8 = 34;
+pub const COLOR_MAGENTA: u8 = 35;
+pub const COLOR_CYAN: u8 = 36;
+pub const COLOR_WHITE: u8 = 37;
+
+pub const COLOR_BRIGHT_RED: u8 = 91;
+pub const COLOR_BRIGHT_GREEN: u8 = 92;
+pub const COLOR_BRIGHT_YELLOW: u8 = 93;
+pub const COLOR_BRIGHT_BLUE: u8 = 94;
+pub const COLOR_BRIGHT_MAGENTA: u8 = 95;
+pub const COLOR_BRIGHT_CYAN: u8 = 96;
+pub const COLOR_BRIGHT_WHITE: u8 = 97;
